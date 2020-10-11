36888d12bac3756dcba9df3a6a18018fefa1588d
neslimanka <43746150+neslimanka@users.noreply.github.com>
2020-10-11 21:59:22 +0300

    .

--- Startup.cs
+++ Startup.cs
@@ -15,19 +15,16 @@ namespace MovieApp
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-    
-        {
+           public void ConfigureServices(IServiceCollection services)
+{            
+    services.AddMvc(option => option.EnableEndpointRouting = false);
+}
 
-          services.AddMvc()
-            .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
-
-
-        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();   //wwwroot
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
