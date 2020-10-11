ac185db0efdd8c9736d26ba71a1cdd40bd287541
neslimanka <43746150+neslimanka@users.noreply.github.com>
2020-10-11 21:11:33 +0300

    .
    ...

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApp.Controllers
+{
+    public class HomeController:Controller
+    {
+        
+
+        public IActionResult Index(){
+
+              return View();
+        }
+        public IActionResult Contact(){
+
+              return View();
+        }
+    }
+}
---
+++ Controllers/MovieController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApp.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        public IActionResult Details()
+        {
+
+            return View();
+        }
+        public IActionResult List()
+        {
+
+            return View();
+        }
+    }
+}
